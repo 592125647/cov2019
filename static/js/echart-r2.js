36dfa1db03914999f4b3ce0b1556beb06b3fdd9f
huyinhao <1457018228@qq.com>
2020-03-11 22:01:08 +0800

    页面右下布局

---
+++ static/js/echart-r2.js
@@ -0,0 +1,69 @@
+var ec_c2 = echarts.init(document.getElementById('r2'),'dark')
+ec_r2_option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    legend: {
+        data: ['死亡', '治愈', '新增确诊', '累计确诊'],
+        right: 20
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'value'
+    },
+    yAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    series: [
+        {
+            name: '死亡',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'insideRight'
+            },
+            data: [320, 302, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '治愈',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'insideRight'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '新增确诊',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'insideRight'
+            },
+            data: [150, 212, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '累计确诊',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                show: true,
+                position: 'insideRight'
+            },
+            data: [820, 832, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+};
+ec_c2.setOption(ec_r2_option);
