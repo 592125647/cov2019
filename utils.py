3fa368f222dd3e72e9b4778725100d04f4e333d1
huyinhao <1457018228@qq.com>
2020-03-12 15:58:33 +0800

    实现历史数据的插入和更新、更新当日疫情、更新国外疫情

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import time
+import traceback
 import pymysql
+from spider import *
 
 
 # 获取时间
@@ -28,6 +30,124 @@ def close_conn(conn, cursor):
         cursor.close()
     if conn:
         conn.close()
+
+
+#  插入历史数据
+def insert_history():
+    """
+    插入历史数据
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        dic = get_tencent_data()[0]  # 0是历史数据，1是当日更新具体数据, 2是外国数据
+        print(f'{time.asctime()}开始插入历史数据')
+        conn, cursor = get_conn()
+        sql = 'insert into history values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        for k, v in dic.items():
+            # item{'2020-1-20':{'confirm':,'suspect':,'heal':,'dead':}}
+            cursor.execute(sql, [k, v.get('confirm'), v.get('confirm_add'),
+                                 v.get('suspect'), v.get('suspect_add'),
+                                 v.get('heal'), v.get('heal_add'),
+                                 v.get('dead'), v.get('dead_add')
+                                 ])
+
+        conn.commit()  # 提交事务
+        print(f'{time.asctime()}插入历史数据完毕')
+    except:
+        traceback.print_exc()
+
+    finally:
+        close_conn(conn, cursor)
+
+
+# 更新历史数据
+def update_history():
+    """
+    更新历史数据
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        dic = get_tencent_data()[0]   # 0是历史数据，1是当日更新具体数据, 2是外国数据
+        print(f'{time.asctime()}开始更新历史数据')
+        conn, cursor = get_conn()
+        sql = 'insert into history values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        sql_query = 'select confirm from history where ds=%s'
+        for k, v in dic.items():
+            # item{'2020-1-20':{'confirm':,'suspect':,'heal':,'dead':}}
+            if not cursor.execute(sql_query, k):
+                cursor.execute(sql, [k, v.get('confirm'), v.get('confirm_add'),
+                                     v.get('suspect'), v.get('suspect_add'),
+                                     v.get('heal'), v.get('heal_add'),
+                                     v.get('dead'), v.get('dead_add')
+                                     ])
+
+        conn.commit()  # 提交事务
+        print(f'{time.asctime()}历史数据更新完毕')
+    except:
+        traceback.print_exc()
+
+    finally:
+        close_conn(conn, cursor)
+
+
+# 更新当日各城市数据
+def update_details():
+    '''
+    更新details表
+    '''
+
+    cursor = None
+    conn = None
+    try:
+        li = get_tencent_data()[1]  # 0是历史数据，1是当日更新具体数据, 2是外国数据
+        conn, cursor = get_conn()
+        sql = 'insert into details(update_time,province,city,confirm,confirm_add,heal,dead) values(%s,%s,%s,%s,%s,%s,%s)'
+        sql_query = 'select %s=(select update_time from details order by id desc limit 1)'
+        cursor.execute(sql_query, li[0][0])
+        if not cursor.fetchone()[0]:
+            print(f'{time.asctime()}开始更新最新数据')
+            for item in li:
+                cursor.execute(sql, item)
+            conn.commit()  # 提交事务
+            print(f'{time.asctime()}最新数据更新完毕')
+        else:
+            print(f'{time.asctime()}已是最新数据')
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+
+# 更新外国数据
+def update_fforeign():
+    """
+    插入国外数据
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        li = get_tencent_data()[2]  # 0是历史数据，1是当日更新具体数据, 2是外国数据
+        conn, cursor = get_conn()
+        sql = 'insert into fforeign(update_time,country,confirm,confirm_add,suspect,heal,dead) values(%s,%s,%s,%s,%s,%s,%s)'
+        sql_query = 'select %s=(select update_time from fforeign order by id desc limit 1)'
+        cursor.execute(sql_query, li[0][0])
+        if not cursor.fetchone()[0]:
+            print(f'{time.asctime()}开始更新最新数据')
+            for item in li:
+                cursor.execute(sql, item)
+            conn.commit()  # 提交事务
+            print(f'{time.asctime()}最新数据更新完毕')
+        else:
+            print(f'{time.asctime()}已是最新数据')
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
 
 
 # sql查询
@@ -133,6 +253,9 @@ def get_r2_data():
 
 if __name__ == '__main__':
     # 测试sql查询返回的数据
+    # update_details()
+    # update_history()
+    # update_fforeign()
     data = get_c2_data()
     print(data)
 
