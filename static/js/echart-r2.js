15992c2bf2003b727eb0e0cc862d05449add0bd7
huyinhao <1457018228@qq.com>
2020-03-14 14:39:43 +0800

    修改一些细节

--- static/js/echart-r2.js
+++ static/js/echart-r2.js
@@ -56,53 +56,6 @@ ec_r2_option = {
             type: 'bar',
             data: [165, 170, 30],
             label: seriesLabel,
-            markPoint: {
-                symbolSize: 1,
-                symbolOffset: [0, '50%'],
-                label: {
-                    formatter: '{a|累计确诊}\n{b|{b} }{c|{c}}',
-                    backgroundColor: 'rgb(242,242,242)',
-                    borderColor: '#aaa',
-                    borderWidth: 1,
-                    borderRadius: 4,
-                    padding: [4, 10],
-                    lineHeight: 26,
-                    shadowBlur: 15,
-                    shadowColor: '#000',
-                    shadowOffsetX: 0,
-                    shadowOffsetY: 1,
-                    position: 'right',
-                    distance: 20,
-                    width: 80,
-                    height:45,
-                    rich: {
-                        a: {
-                            align: 'center',
-                            color: '#fff',
-                            fontSize: 16,
-                            textShadowBlur: 2,
-                            textShadowColor: '#000',
-                            textShadowOffsetX: 0,
-                            textShadowOffsetY: 1,
-                            textBorderColor: '#333',
-                            textBorderWidth: 2
-                        },
-                        b: {
-                            color: '#333',
-                            fontSize:14
-                        },
-                        c: {
-                            color: '#ff8811',
-                            textBorderColor: '#000',
-                            textBorderWidth: 1,
-                            fontSize: 16
-                        }
-                    }
-                },
-                data: [
-                    {type: 'max', name: 'max:'},
-                ]
-            }
         },
         {
             name: '累计治愈',
